ab7729e51805e698fdb3768754c95a887f690dc9
63shane63 <63shane63@gmail.com>
2025-08-27 22:16:44 -0400

    adding script for result input, sizing instantiated buttons

--- Assets/Scripts/Pick_autosizing.cs
+++ Assets/Scripts/Pick_autosizing.cs
@@ -4,22 +4,42 @@ using UnityEngine.UI;
 [RequireComponent(typeof(GridLayoutGroup))]
 public class AutoCellSize : MonoBehaviour
 {
-    public int columns = 4;
-    public int rows = 5;
-    public Vector2 spacing = new Vector2(5, 5);
+    public Vector2 spacing = new Vector2(200, 100);
 
-    void Start() => UpdateCellSize();
-    void OnRectTransformDimensionsChange() => UpdateCellSize();
+    private GridLayoutGroup grid;
+    private RectTransform rt;
 
-    void UpdateCellSize()
+    private void Awake()
     {
-        var grid = GetComponent<GridLayoutGroup>();
-        var rt = GetComponent<RectTransform>();
+        grid = GetComponent<GridLayoutGroup>();
+        rt = GetComponent<RectTransform>();
+    }
+
+    private void OnEnable()
+    {
+        UpdateCellSize();
+    }
+
+    private void OnRectTransformDimensionsChange()
+    {
+        UpdateCellSize();
+    }
+
+    private void UpdateCellSize()
+    {
+        var race = GameManager.I.GetSelectedRace();
+        if (race == null) return;
+
+        int totalButtons = 20; // or dynamically count children if needed
+        int columns = 2;
+        int rows = race.type == RaceType.Feature ? 10 : 5;
 
         float cellWidth = (rt.rect.width - (spacing.x * (columns - 1))) / columns;
         float cellHeight = (rt.rect.height - (spacing.y * (rows - 1))) / rows;
 
         grid.cellSize = new Vector2(cellWidth, cellHeight);
         grid.spacing = spacing;
+        grid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        grid.constraintCount = columns;
     }
 }
