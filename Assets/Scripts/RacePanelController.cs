bebd3550f75e55d3440fee27d0405a6dcf84a5f5
63shane63 <63shane63@gmail.com>
2025-09-01 16:16:56 -0400

    Fixed the race counter and remove race button

--- Assets/Scripts/RacePanelController.cs
+++ Assets/Scripts/RacePanelController.cs
@@ -1,7 +1,7 @@
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class RacePanelController : MonoBehaviour
 {
@@ -19,7 +19,17 @@ public class RacePanelController : MonoBehaviour
     {
         var type = (RaceType)raceTypeDropdown.value;
         var night = GameManager.I.State.nights[GameManager.I.State.currentNightIndex];
-        string displayName = $"{type} {night.races.Count + 1}";
+
+        // Count only races of this type
+        int typeCount = 0;
+        foreach (var r in night.races)
+        {
+            if (r.type == type)
+                typeCount++;
+        }
+
+        string displayName = $"{type} {typeCount + 1}";
+
         GameManager.I.AddRaceToCurrentNight(type, displayName);
         GameManager.I.Save();
         RefreshList();
@@ -29,28 +39,68 @@ public class RacePanelController : MonoBehaviour
     {
         var raceNightNumber = GameManager.I.State.currentNightIndex;
         raceNightText.text = $"Event Night {raceNightNumber + 1}";
-        var currentnight = GameManager.I.State.nights[GameManager.I.State.currentNightIndex];
-        if (currentnight.finished != true)
-        {
-            AddRaceButton.interactable = true;
-        }
-        foreach (Transform c in raceListContent) Destroy(c.gameObject);
-        var night = GameManager.I.State.nights[GameManager.I.State.currentNightIndex];
 
-        for (int i = 0; i < night.races.Count; i++)
+        var currentNight = GameManager.I.State.nights[GameManager.I.State.currentNightIndex];
+        AddRaceButton.interactable = !currentNight.finished;
+
+        // Clear old list
+        foreach (Transform c in raceListContent) Destroy(c.gameObject);
+
+        // Track numbering per race type
+        Dictionary<RaceType, int> typeCounters = new Dictionary<RaceType, int>();
+
+        for (int i = 0; i < currentNight.races.Count; i++)
         {
             int raceIdx = i;
-            var race = night.races[i];
+            var race = currentNight.races[i];
+
+            // Increment counter for this race type
+            if (!typeCounters.ContainsKey(race.type))
+                typeCounters[race.type] = 0;
+            typeCounters[race.type]++;
+
+            // Generate the correct display name
+            string displayName = $"{race.type} {typeCounters[race.type]}";
+
+            // Update the actual race object so GameManager sees the new name
+            race.displayName = displayName;
+
             var go = Instantiate(raceButtonPrefab, raceListContent);
+
+            // Label
             var label = go.GetComponentInChildren<TMP_Text>();
-            if (label) label.text = race.displayName;
+            if (label) label.text = displayName;
+
+            // Main select button
             var btn = go.GetComponent<Button>();
             if (btn) btn.onClick.AddListener(() =>
             {
                 GameManager.I.SelectRace(raceIdx);
                 ui.Show(PanelId.Player_Panel);
             });
+
+            // Remove button wiring
+            var removeBtn = go.transform.Find("RemoveButton")?.GetComponent<Button>();
+            if (removeBtn != null)
+            {
+                removeBtn.onClick.AddListener(() =>
+                {
+                    confirmDialog.Show(
+                        "Remove this race?",
+                        onConfirm: () =>
+                        {
+                            currentNight.races.RemoveAt(raceIdx);
+                            GameManager.I.Save();
+                            RefreshList(); // Renumber after removal
+                        },
+                        onCancel: RefreshList
+                    );
+                });
+            }
         }
+
+        // Save after renaming so changes persist
+        GameManager.I.Save();
     }
 
     public void OnBack() => ui.Show(PanelId.Event_Panel);
@@ -59,9 +109,6 @@ public class RacePanelController : MonoBehaviour
 
     public void OnClickConfirm()
     {
-        // Optional: validate inputs before showing dialog
-        // if (!IsResultsComplete()) { /* show message */ return; }
-
         confirmDialog.Show(
             "Are You Sure?",
             onConfirm: OnFinishNight,
@@ -74,11 +121,7 @@ public class RacePanelController : MonoBehaviour
         var night = GameManager.I.State.nights[GameManager.I.State.currentNightIndex];
         night.finished = true;
         GameManager.I.Save();
-        if (AddRaceButton != null)
-        {
-            AddRaceButton.interactable = false; 
-        }
-
+        AddRaceButton.interactable = false;
         ui.Show(PanelId.Event_Panel);
     }
 }
